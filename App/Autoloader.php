69beff32708187736b6df43404fcfb28b741fa32
Simon Balleux <simoncestmoi@hotmail.fr>
2022-10-24 12:25:59 +0200

    Creation de ma structure MVC + autoload

---
+++ App/Autoloader.php
@@ -0,0 +1,19 @@
+<?php
+
+// Définir le namespace
+namespace App;
+
+
+class Autoloader {
+    static function spl_register() {
+        spl_autoload_register(array(__CLASS__, 'autoload'));
+    }
+
+    static function autoload($class) {
+        if (strpos($class, __NAMESPACE__ . '\\') === 0) {
+            $class = str_replace(__NAMESPACE__ . '\\', '', $class);
+            $class = str_replace('\\', '/', $class);
+            require __DIR__ . '/' . $class . ' .php';
+        }
+    }
+}
